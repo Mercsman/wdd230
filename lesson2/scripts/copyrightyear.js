1e3ae3a0fa60e73f5bc7a0b993b3d98401002bbe
mercsman <tropicalxfury@gmail.com>
2023-01-14 21:37:03 -0800

    scripts/header/footer

---
+++ lesson2/scripts/copyrightyear.js
@@ -0,0 +1,7 @@
+const year = new Date();
+const copyright = year.getFullYear();
+document.getElementById('copyright').innerHTML = 'WDD230 Projects &copy; ' + copyright + ' | Seth Bowles | California';
+// document.querySelector
+
+// const copyright = document.getElementById('copyright').innerHTML;
+// copyright = document.write(new Date().getFullYear()).innerHTML;
---
+++ lesson2/scripts/lastmodified.js
@@ -0,0 +1,3 @@
+const lastmod = document.lastModified;
+
+document.getElementById('modified').innerHTML = 'Last modified ' + lastmod;
