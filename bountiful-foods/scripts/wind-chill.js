789638f33f5d18e61217337931fd1789ae3420f1
mercsman <tropicalxfury@gmail.com>
2023-04-05 14:57:41 -0700

    fixed weather

--- bountiful-foods/scripts/wind-chill.js
+++ bountiful-foods/scripts/wind-chill.js
@@ -46,7 +46,7 @@ const humidity2 = document.querySelector('#humidity2');
 
 async function apiFetch2() {
 
-    const url2 = 'https://api.openweathermap.org/data/2.5/weather?lat=33.15&lon=-117.4&units=imperial&cnt=24&appid=11db6f75b42e50b8f59e0e5ea2f6bea5';
+    const url2 = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.15&lon=-117.4&units=imperial&cnt=24&appid=11db6f75b42e50b8f59e0e5ea2f6bea5';
     try{
     const response = await fetch(url2);
     if (response.ok) {
@@ -65,10 +65,10 @@ apiFetch2();
 
 
 function displayResults2(weatherData) {
-    currentTemp2.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
-    const iconsrc2 = `https://openweathermap.org/img/w/${weatherData.weather[1].icon}.png`;
-    const desc2 = weatherData.weather[1].description;
-    humidity2.innerHTML = weatherData.main.humidity;
+    currentTemp2.innerHTML = `<strong>${weatherData.list[8].main.temp.toFixed(0)}</strong>`;
+    const iconsrc2 = `https://openweathermap.org/img/w/${weatherData.list[8].weather[0].icon}.png`;
+    const desc2 = weatherData.list[8].weather[0].description;
+    humidity2.innerHTML = weatherData.list[8].main.humidity;
 
     weatherIcon2.setAttribute('src', iconsrc2);
     weatherIcon2.setAttribute('alt', desc2);
@@ -86,7 +86,7 @@ const humidity3 = document.querySelector('#humidity3');
 
 async function apiFetch3() {
 
-    const url3 = 'https://api.openweathermap.org/data/2.5/weather?lat=33.15&lon=-117.4&units=imperial&cnt=24&appid=11db6f75b42e50b8f59e0e5ea2f6bea5';
+    const url3 = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.15&lon=-117.4&units=imperial&cnt=24&appid=11db6f75b42e50b8f59e0e5ea2f6bea5';
     try{
     const response = await fetch(url3);
     if (response.ok) {
@@ -105,10 +105,10 @@ apiFetch3();
 
 
 function displayResults3(weatherData) {
-    currentTemp3.innerHTML = `<strong>${weatherData.main.temp.toFixed(2)}</strong>`;
-    const iconsrc3 = `https://openweathermap.org/img/w/${weatherData.weather[2].icon}.png`;
-    const desc3 = weatherData.weather[2].description;
-    humidity3.innerHTML = weatherData.main.humidity;
+    currentTemp3.innerHTML = `<strong>${weatherData.list[16].main.temp.toFixed(0)}</strong>`;
+    const iconsrc3 = `https://openweathermap.org/img/w/${weatherData.list[16].weather[0].icon}.png`;
+    const desc3 = weatherData.list[16].weather[0].description;
+    humidity3.innerHTML = weatherData.list[16].main.humidity;
 
     weatherIcon3.setAttribute('src', iconsrc3);
     weatherIcon3.setAttribute('alt', desc3);
